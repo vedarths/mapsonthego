11d700ab0720217e2619b369dd58632cb34a9d9b
Sudi <vedarthlimited@gmail.com>
2018-11-10 09:29:03 +0000

    added more UI elements with constraints

---
+++ ParseClient.swift
@@ -0,0 +1,9 @@
+//
+//  ParseClient.swift
+//  MyVMapOnTheGo
+//
+//  Created by Vedarth Solutions on 11/10/18.
+//  Copyright © 2018 Vedarth Solutions. All rights reserved.
+//
+
+import Foundation
