de8f42ad4b11889ac0f18f59e41953d19c4d78ab
Sudi <vedarthlimited@gmail.com>
2019-02-02 15:40:44 +0000

    More ui changes

---
+++ MyVMapOnTheGo/MyVMapOnTheGo/Controller/FindLocationController.swift
@@ -0,0 +1,14 @@
+//
+//  FindLocationController.swift
+//  MyVMapOnTheGo
+//
+//  Created by Vedarth Solutions on 2/2/19.
+//  Copyright © 2019 Vedarth Solutions. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class FindLocationController: UIViewController, UITextFieldDelegate {
+    
+}
